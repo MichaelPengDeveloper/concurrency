7baa30f0913995b3caf40965355987ac2be8f73f
MichaelPengDeveloper <www.277214756@qq.com>
2018-06-12 15:39:38 +0800

    fix

---
+++ src/main/java/com/demo/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,19 @@
+package com.demo.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【线程安全】的注解
+ *
+ * Created by Wangpeng on 2018/6/12.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+
+}
